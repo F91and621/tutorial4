f383246a7ced1dfd1cc6c1a52454e7760ca63e7e
F91 <1623721391@qq.com>
2025-10-16 17:09:58 +0800

    update

--- src/test/java/org/example/TestSubtraction.java
+++ src/test/java/org/example/TestSubtraction.java
@@ -3,12 +3,11 @@ package org.example;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class CalcTest {
-  Calc c = new Calc();
-
+public class TestSubtraction {
+    Calc c = new Calc();
     @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
+    void subtraction(){
+        assertEquals(2, c.subtract(4,2));
     }
 
 }
